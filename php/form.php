8d1da41bcaa8fdec51fba7da62957540034b9cb1
Vedant1202 <Vedant1202@users.noreply.github.com>
2018-09-28 00:49:42 +0530

    Added form

--- php/form.php
+++ php/form.php
@@ -43,48 +43,50 @@
     <link rel="stylesheet" href="../stylesheets/form.css">
     <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> -->
 
-    <div class="jumbotron">
-      <h2>Form</h2>
-      <p>
-        <div class="alert alert-error">
-          <?= $_SESSION['message'] ?>
+
+    <div class="container" align="center">
+      <div class="jumbotron">
+        <h2>Form</h2>
+        <p>
+          <div class="alert alert-error">
+            <?= $_SESSION['message'] ?>
+          </div>
+        </p>
+      </div>
+
+      <form class="login" action="form.php" method="post">
+        <!-- <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+          <input type="text" class="form-control" id="exampleInputEmail1" name="username" aria-describedby="emailHelp" placeholder="Enter email">
+          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
         </div>
-      </p>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input type="text" class="form-control" id="exampleInputPassword1" name="email" placeholder="Password">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button> -->
+
+
+  <!-- //////////////////////////////// -->
+
+      <!-- <form class="login"> -->
+        <p class="title">Log in</p>
+        <input type="text" placeholder="Username" autofocus name="username"/>
+          <i class="fa fa-user"></i>
+        <input type="password" placeholder="password" name = 'password'/>
+          <i class="fa fa-key"></i>
+        <!-- <a href="#">Forgot your password?</a> -->
+        <button type="submit">
+          <i class="spinner"></i>
+          <span class="state">Log in</span>
+        </button>
+      </form>
+
     </div>
-
-    <form class="login" action="form.php" method="post">
-      <!-- <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input type="text" class="form-control" id="exampleInputEmail1" name="username" aria-describedby="emailHelp" placeholder="Enter email">
-        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-      </div>
-      <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input type="text" class="form-control" id="exampleInputPassword1" name="email" placeholder="Password">
-      </div>
-      <button type="submit" class="btn btn-primary">Submit</button> -->
-
-
-<!-- //////////////////////////////// -->
-
-    <!-- <form class="login"> -->
-      <p class="title">Log in</p>
-      <input type="text" placeholder="Username" autofocus name="username"/>
-        <i class="fa fa-user"></i>
-      <input type="password" placeholder="password" name = 'password'/>
-        <i class="fa fa-key"></i>
-      <!-- <a href="#">Forgot your password?</a> -->
-      <button type="submit">
-        <i class="spinner"></i>
-        <span class="state">Log in</span>
-      </button>
-    </form>
-
-    </form>
 
 
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script type="text/javascript" src="../scripts/form.js"></script>
+    <!-- <script type="text/javascript" src="../scripts/form.js"></script> -->
     <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
     <!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script> -->
     <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script> -->
