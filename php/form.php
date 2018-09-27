65de4238c9a9ed2ad52b21ecd90c1c93ad4ebc1c
Vedant1202 <Vedant1202@users.noreply.github.com>
2018-09-28 00:56:01 +0530

    Added form

--- php/form.php
+++ php/form.php
@@ -46,6 +46,8 @@
 
     <div class="container" align="center">
 
+
+
       <form class="login" action="form.php" method="post">
         <!-- <div class="form-group">
           <label for="exampleInputEmail1">Email address</label>
@@ -60,7 +62,9 @@
 
 
   <!-- //////////////////////////////// -->
-
+      <div class="alert alert-error">
+        <?= $_SESSION['message'] ?>
+      </div>
       <!-- <form class="login"> -->
         <p class="title">Log in</p>
         <input type="text" placeholder="Username" autofocus name="username"/>
@@ -78,7 +82,7 @@
 
 
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <!-- <script type="text/javascript" src="../scripts/form.js"></script> -->
+    <script type="text/javascript" src="../scripts/form.js"></script>
     <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script> -->
