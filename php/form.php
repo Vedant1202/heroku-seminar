07f10cbd639a576672d83ef396ade4ea8c560cad
Vedant1202 <Vedant1202@users.noreply.github.com>
2018-09-28 23:14:40 +0530

    fixed bugs

--- php/form.php
+++ php/form.php
@@ -58,7 +58,7 @@
       </div>
       <div class="form-group">
         <label for="exampleInputPassword1">Password</label>
-        <input type="text" class="form-control" id="exampleInputPassword1" name="password" placeholder="Password">
+        <input type="password" class="form-control" id="exampleInputPassword1" name="password" placeholder="Password">
       </div>
       <button type="submit" class="btn btn-primary">Submit</button>
 
