511e1131dbf5f5264fd8700188c029e1cf3768f8
Vedant1202 <Vedant1202@users.noreply.github.com>
2018-09-28 22:25:55 +0530

    fixed bugs

--- php/form.php
+++ php/form.php
@@ -69,7 +69,7 @@
         <p class="title">Log in</p>
         <input type="text" placeholder="Username" name="username">
           <i class="fa fa-user"></i>
-        <input type="password" placeholder="Password" name='password'>
+        <input type="text" placeholder="Password" name='password'>
           <i class="fa fa-key"></i>
         <!-- <a href="#">Forgot your password?</a> -->
         <button type="submit">
