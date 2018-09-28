7fe0af5ba5c0ef5fc70b499155b2cc68c6ee02a7
Vedant1202 <Vedant1202@users.noreply.github.com>
2018-09-28 22:28:36 +0530

    fixed bugs

--- php/form.php
+++ php/form.php
@@ -39,8 +39,8 @@
   </head>
   <body>
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="../stylesheets/form.css">
+    <!-- <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css"> -->
+    <!-- <link rel="stylesheet" href="../stylesheets/form.css"> -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
 
